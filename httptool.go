f6e765deb0696017b308ff85d430998a78ada7ac
隋宗桂 <suizonggui@inke.cn>
2019-04-18 15:22:48 +0800

    init

---
+++ httptool.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func httpDo(url string, header map[string]string, httpMethod string, httpPostBody string) []byte {
+
+	client := &http.Client{}
+	req, err := http.NewRequest(strings.ToUpper(httpMethod), url, strings.NewReader(httpPostBody))
+	if err != nil {
+
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+
+	}
+
+	fmt.Println(string(body))
+	return body
+}
